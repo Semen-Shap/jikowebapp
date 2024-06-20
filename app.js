ea0690480b972ec3ca88465da3459bc4546f37e4
Semyon Shapoval <Sem778shapoval@gmail.com>
2024-06-20 16:22:51 +0700

    test

--- app.js
+++ app.js
@@ -5,6 +5,28 @@ tg.expand();
 let btn = document.getElementById('btn')
 
 btn.addEventListener('click', function(){
+    
     tg.close();
 });
 
+var MainButton = WebApp.MainButton;
+var BackButton = WebApp.BackButton;
+
+MainButton.show();
+BackButton.show();
+
+MainButton.onClick(function() {
+  WebApp.showAlert("Хорошо, ты нажал на главную кнопку.");
+});
+WebApp.onEvent('mainButtonClicked', function() {
+  /* also */
+});
+
+BackButton.onClick(function() {
+  WebApp.showAlert("Нет пути назад!");
+  
+  BackButton.hide();
+});
+WebApp.onEvent('backButtonClicked', function() {
+  /* also */
+});
